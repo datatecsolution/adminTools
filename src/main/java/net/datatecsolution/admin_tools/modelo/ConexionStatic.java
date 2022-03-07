ca26d0562bb711775620bca783ecd49046bd79e5
Jose David Mayorga <jdmayorga@Mac-mini-de-Jose.local>
2022-03-07 13:56:13 -0600

    correcion en el reporte da la factura

--- src/main/java/net/datatecsolution/admin_tools/modelo/ConexionStatic.java
+++ src/main/java/net/datatecsolution/admin_tools/modelo/ConexionStatic.java
@@ -30,7 +30,7 @@ public abstract class ConexionStatic implements Runnable{
 
 	private static String bd = "admin_tools";
 
-
+/*
 	//LOCAL_PRODUCCION
 	//private static String login = "admin";
 	private static String login = "root";
@@ -69,7 +69,7 @@ public abstract class ConexionStatic implements Runnable{
 	private static String login = "admin";
 	private static String password = "Jdmm123?";
 	private static String server = "192.168.1.101";
-
+*/
 	//Hotel Venecia
 	private static String login = "admin";
 	private static String password = "Jdmm123?";
