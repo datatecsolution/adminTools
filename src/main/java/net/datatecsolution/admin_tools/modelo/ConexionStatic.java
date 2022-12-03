73bc2170826edb3917ecb059be8b05bb8e3ba1a7
Jose David Mayorga <jdmayorga@Mac-mini-de-Jose.local>
2022-12-03 16:09:29 -0600

    conexion

--- src/main/java/net/datatecsolution/admin_tools/modelo/ConexionStatic.java
+++ src/main/java/net/datatecsolution/admin_tools/modelo/ConexionStatic.java
@@ -69,7 +69,7 @@ public abstract class ConexionStatic implements Runnable{
 	private static String login = "admin";
 	private static String password = "Jdmm123?";
 	private static String server = "192.168.1.101";
-*/
+
 	//Hotel Venecia
 	private static String login = "admin";
 	private static String password = "Jdmm123?";
@@ -91,13 +91,13 @@ public abstract class ConexionStatic implements Runnable{
 	private static String password = "ronLsnta123.";
 	private static String server = "192.168.10.102";
 
-
+*/
 	//MISCELANIAS W&C
 	private static String login = "user_pos";
 	private static String password = "Admin123.";
 	private static String server = "192.168.1.25";
 
-
+/*
 
 	//FERROCENTER           
 	private static String login = "user_admin";
